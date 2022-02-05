deb76157acbf0072d50c3a7961e3ebb0ff6cb973
Nagarajking1 <97332888+Nagarajking1@users.noreply.github.com>
2022-02-05 21:52:26 +0530

    Update __init__.py

--- shizuka/__init__.py
+++ shizuka/__init__.py
@@ -2,8 +2,8 @@ import os
 
 from pyrogram import Client
 
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-TOKEN = os.environ.get("TOKEN", None)
+API_ID = os.environ.get("7678244", None)
+API_HASH = os.environ.get("e23d94ebe8749ebacabf19d597e0d94f", None)
+TOKEN = os.environ.get("1955521660:AAGpUTiMawjm2FGAGLYOp-_bHWj8ZMZbrHc", None)
 
 SHIZUKA = Client(":memory:", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
